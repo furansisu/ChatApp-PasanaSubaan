aa81b76a331c490c9adf2f1466799e59d301ec48
samkayes <samanthasubaan@gmail.com>
2022-06-07 23:38:56 +0800

    additions
    not tested yet

---
+++ ChatApp-PasanaSubaan/ChatApp-PasanaSubaan/DependencyServices/iFirebaseAuth.cs
@@ -0,0 +1,17 @@
+﻿using ChatApp_PasanaSubaan.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp_PasanaSubaan.DependencyServices
+{
+    interface iFirebaseAuth
+    {
+        Task<FirebaseAuthResponseModel> LoginWithEmailPassword(string email, string password);
+        Task<FirebaseAuthResponseModel> SignUpWithEmailPassword(string name, string email, string password);
+        FirebaseAuthResponseModel SignOut();
+        FirebaseAuthResponseModel IsLoggedIn();
+        Task<FirebaseAuthResponseModel> ResetPassword(string email);
+    }
+}
--- ChatApp-PasanaSubaan/ChatApp-PasanaSubaan/TabbedPage1.xaml.cs
+++ ChatApp-PasanaSubaan/ChatApp-PasanaSubaan/TabbedPage1.xaml.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ChatApp_PasanaSubaan.Models;
 
 namespace ChatApp_PasanaSubaan
 {
     public partial class TabbedPage1 : TabbedPage
     {
+        DataClass dataClass = DataClass.GetInstance;
         public TabbedPage1()
         {
             InitializeComponent();
+            //ProfilePage.name = dataClass.loggedInUser.name;
+            //ProfilePage.email = dataClass.loggedInUser.email;
         }
     }
 }
