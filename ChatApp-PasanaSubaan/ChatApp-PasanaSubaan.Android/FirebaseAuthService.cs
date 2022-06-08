ffb9a213a727f6b604271644e52295067f37ab3c
samkayes <samanthasubaan@gmail.com>
2022-06-08 22:26:52 +0800

    idk

---
+++ ChatApp-PasanaSubaan/ChatApp-PasanaSubaan.Android/FirebaseAuthService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using Plugin.CloudFirestore;
+using ChatApp_PasanaSubaan.Droid;
+using Xamarin.Forms;
+using ChatApp_PasanaSubaan.Models;
+
+[assembly: Dependency(typeof(FirebaseAuthService))]
+
+namespace ChatApp_PasanaSubaan.Droid
+{
+    public class FirebaseAuthService : iFirebaseAuth
+    {
+        DataClass dataClass = DataClass.GetInstance;
+
+        public FirebaseAuthResponseModel IsLoggedIn()
+        {
+
+        }
+    }
+}
